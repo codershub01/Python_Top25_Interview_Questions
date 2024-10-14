7d04af0eb766845a279a274c5cd615a7f20be663
nufidev <devrel@nuchain.co>
2024-10-14 15:53:55 +0530

    q6:

---
+++ 06_sort_a_list.py
@@ -0,0 +1,19 @@
+## Sorting a given list using pre-defined methods
+
+## Method-1 :: sort()
+
+def sort_list(lst):
+
+    lst.sort()
+    print(lst)
+
+sort_list([65,98,23,55,2,4,61,87,49])
+
+## Method-2 :: sorted()
+
+def sort_list(lst):
+
+    lst = sorted(lst)
+    print(lst)
+
+sort_list([65,98,23,55,2,4,61,87,49])
